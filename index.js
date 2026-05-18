05edb80a2963c81760c7171367b5c275ae5540ef
jlyszkowski <jlyszkowski1984@gmail.com>
2026-05-18 19:12:06 -0400

    Added js

---
+++ index.js
@@ -0,0 +1,25 @@
+// template_cujp4wj
+// service_glnq3cw
+// MVjAAWAVuG8mqgZTJ
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+     .sendForm(
+        "service_glnq3cw",
+        "template_cujp4wj",
+        event.target,
+        "MVjAAWAVuG8mqgZTJ"
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible"; 
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+         "The email service is temporarily unavailable. Please contact me directly at jlyszkowski1984@gmail.com"
+        );
+    })
+}
