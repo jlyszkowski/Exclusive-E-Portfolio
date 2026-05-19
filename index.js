84db81b1c1524d3a6d2c24b462bf5da4463ad93b
jlyszkowski <jlyszkowski1984@gmail.com>
2026-05-19 19:23:18 -0400

    updated 05/19/2026

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// template_cujp4wj
-// service_glnq3cw
-// MVjAAWAVuG8mqgZTJ
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += "dark-theme";
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -22,4 +30,13 @@ function contact(event) {
          "The email service is temporarily unavailable. Please contact me directly at jlyszkowski1984@gmail.com"
         );
     })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
